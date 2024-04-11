e63a4bbf5e1c824ff91cb39efdce6a908b576ee6
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-04-11 18:30:58 +0900

    Feat: add Map move logic

--- src/containers/Map/index.tsx
+++ src/containers/Map/index.tsx
@@ -11,18 +11,18 @@ import MapTest3 from '@/assets/images/map-test3.jpg';
 
 export default function Map() {
   const mapTestData = [
-    {
-      id: 1,
-      staticImageData: MapTest1,
-      imageWidth: 1965,
-      imageHeight: 1275,
-    },
-    {
-      id: 2,
-      staticImageData: MapTest2,
-      imageWidth: 3401,
-      imageHeight: 8192,
-    },
+    // {
+    //   id: 1,
+    //   staticImageData: MapTest1,
+    //   imageWidth: 1965,
+    //   imageHeight: 1275,
+    // },
+    // {
+    //   id: 2,
+    //   staticImageData: MapTest2,
+    //   imageWidth: 3401,
+    //   imageHeight: 8192,
+    // },
     {
       id: 3,
       staticImageData: MapTest3,
@@ -83,6 +83,7 @@ function MapAreaTest (
     imageHeight: number;
   }
 ) {
+  // Zoom 기능 - scale, wheel 관련
   const IMAGE_SCALE_UNIT = 0.4;
   const MIN_IMAGE_SCALE = 1;
 
@@ -91,6 +92,20 @@ function MapAreaTest (
   const imageScaleRef = useRef<number>(MIN_IMAGE_SCALE);
 
   const [imageScale, setImageScale] = useState<number>(MIN_IMAGE_SCALE);
+
+  // Move 기능 - mouseDown, mouseMove, mouseUp 관련
+  const MIN_MOVE_DISTANCE = 4;
+
+  const moveStartPointX = useRef<number>(0);
+  const moveStartPointY = useRef<number>(0);
+  const finalMovePointX = useRef<number>(0);
+  const finalMovePointY = useRef<number>(0);
+  const prevImagePointX = useRef<number>(0);
+  const prevImagePointY = useRef<number>(0);
+
+  const [imageMovePointX, setImageMovePointX] = useState<number>(0);
+  const [imageMovePointY, setImageMovePointY] = useState<number>(0);
+
 
   useEffect(() => {
     const mapViewerElm = mapViewerRef.current;
@@ -113,6 +128,8 @@ function MapAreaTest (
     maxImageScaleRef.current = Number(largestNum.toFixed(1));
   }
 
+  // TODO 추가로 생각해야할 이슈
+  // 첫 Scale 변경 시 느림 - 원인 파악 필요
   function handleWheelMapViewer(e: WheelEvent) {
     e.preventDefault();
 
@@ -132,6 +149,55 @@ function MapAreaTest (
     setImageScale(imageScaleRef.current);
   }
 
+  // TODO 이동 관련해서 생각해 봐야할 이슈
+  // addEventListener, removeEventListener 적절하게 작동하는지 확인 필요 - 필요한만큼(1개겠지) 추가되고 삭제된다.
+  // mouseMove는 아주 많은 이벤트를 발생 시킨다. - 성능 이슈 발생하려나?? + 이를 해결하기 위해 Throttling을 사용한다면... 매끄럽게 보일까?
+  function handleMouseDownMapViewer(e: React.MouseEvent) {
+    moveStartPointX.current = e.clientX;
+    moveStartPointY.current = e.clientY;
+
+    // 맵뷰어에서 mouseMove, mouseUp 위치가 맵뷰어를 넘어갈 수 있기 때문에 window에 이벤트를 건다.
+    window.addEventListener('mousemove', handleMouseMoveWindow);
+    window.addEventListener('mouseup', handleMouseUpWindow);
+  }
+
+  function handleMouseMoveWindow(e: MouseEvent) {
+    const movePointX = Math.round((moveStartPointX.current - e.clientX) / imageScale);
+    const movePointY = Math.round((moveStartPointY.current - e.clientY) / imageScale);
+
+    // 움직인 거리가 아주 작은 값일 경우 click miss 로 판단
+    if (Math.abs(movePointX) < MIN_MOVE_DISTANCE && Math.abs(movePointY) < MIN_MOVE_DISTANCE) {
+      return;
+    }
+
+    // TODO 이미지가 맵 영역 끝에 도달하면 더 이상 이동되면 안된다.
+    // 추가 작업 필요
+
+    finalMovePointX.current = prevImagePointX.current - movePointX;
+    finalMovePointY.current = prevImagePointY.current - movePointY;
+
+    setImageMovePointX(finalMovePointX.current);
+    setImageMovePointY(finalMovePointY.current);
+  }
+
+  function handleMouseUpWindow(e: MouseEvent) {
+    moveStartPointX.current = 0;
+    moveStartPointY.current = 0;
+    prevImagePointX.current = finalMovePointX.current;
+    prevImagePointY.current = finalMovePointY.current;
+    finalMovePointX.current = 0;
+    finalMovePointY.current = 0;
+
+    // 관련된 이벤트를 제거해야 한다.
+    window.removeEventListener('mousemove', handleMouseMoveWindow);
+    window.removeEventListener('mouseup', handleMouseUpWindow);
+  }
+
+
+
+  // TODO 좌표 기능
+
+
   return (
     <>
       <Box
@@ -143,6 +209,7 @@ function MapAreaTest (
       >
         <Box
           ref={mapViewerRef}
+          onMouseDown={handleMouseDownMapViewer}
           sx={{
             position: 'relative',
             width: '100%',
@@ -171,7 +238,7 @@ function MapAreaTest (
                 width: '100%',
                 height: '100%',
                 objectFit: 'contain',
-                transform: `scale(${imageScale})`
+                transform: `scale(${imageScale}) translate(${imageMovePointX}px, ${imageMovePointY}px)`,
               }}
             />
           </Box>
@@ -191,9 +258,11 @@ function MapAreaTest (
           <Box>이미지 세로 : { props.imageHeight }px</Box>
         </Box>
         <Box sx={{ flex: 1 }}>
-          좌표
+          <Box>imageMovePointX : { imageMovePointX }px</Box>
+          <Box>imageMovePointY : { imageMovePointY }px</Box>
         </Box>
         <Box sx={{ flex: 1 }}>
+          좌표 시스템
         </Box>
       </Box>
     </>
