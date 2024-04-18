8fb0b0e97844923eb28730644f836329c92b8567
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-04-18 17:33:28 +0900

    Feat: update tempTestData

--- src/api/tempData.ts
+++ src/api/tempData.ts
@@ -2,43 +2,81 @@
 
 import { ResMap } from './types';
 
-import MapTest1 from '@/assets/images/map-test1.jpg';
-import MapTest2 from '@/assets/images/map-test2.jpg';
-import MapTest3 from '@/assets/images/map-test3.jpg';
+import MapTest1 from '@/assets/images/map-test-1.jpg';
+// import MapTest2 from '@/assets/images/map-test2.jpg';
+// import MapTest3 from '@/assets/images/map-test3.jpg';
 
+// Map 관련
 const resMapData1 = {
   id: 1,
-  name: '테스트 - 에인세르 강',
+  name: '테스트 - 림그레이브 서부',
   imageUrl: MapTest1,
-  width: 1965,
-  height: 1275,
+  width: 3304,
+  height: 4642,
 }
 
-const resMapData2 = {
-  id: 2,
-  name: '테스트 - 리에니에',
-  imageUrl: MapTest2,
-  width: 3401,
-  height: 8192,
-}
+// const resMapData2 = {
+//   id: 2,
+//   name: '테스트 - 리에니에',
+//   imageUrl: MapTest2,
+//   width: 3401,
+//   height: 8192,
+// }
 
-const resMapData3 = {
-  id: 3,
-  name: '테스트 - 케일리드',
-  imageUrl: MapTest3,
-  width: 5292,
-  height: 4926,
-}
+// const resMapData3 = {
+//   id: 3,
+//   name: '테스트 - 케일리드',
+//   imageUrl: MapTest3,
+//   width: 5292,
+//   height: 4926,
+// }
 
 const resMapList: ResMap[] = [
   resMapData1,
-  resMapData2,
-  resMapData3,
+  // resMapData2,
+  // resMapData3,
+];
+
+// Item 관련
+const resTestItems = [
+  {
+    // id: 1,
+    locationType: '던전',
+    icon: '던전아이콘',
+    name: '높은 길 아래 동굴',
+    coord: {
+      x: 19,
+      y: 50
+    },
+    items: [
+      {
+        itemType: '강화재료? 단석?',
+        name: '단석[1]',
+        count: 3,
+      },
+      {
+        itemType: '강화재료? 단석?',
+        name: '단석[2]',
+        count: 1,
+      },
+      {
+        itemType: '무기? 곡검?',
+        name: '샴시르',
+        count: 1,
+      },
+      {
+        itemType: '탈리스만 - 필수? / 반필수?',
+        name: '푸른 무희',
+        count: 1,
+      },
+    ]
+  }
 ];
 
 export {
   resMapData1,
-  resMapData2,
-  resMapData3,
+  // resMapData2,
+  // resMapData3,
   resMapList,
+  resTestItems,
 }
--- src/api/types.ts
+++ src/api/types.ts
@@ -1,6 +1,9 @@
+import { StaticImageData } from 'next/image';
+
 type ResMap = {
   id: number;
   name: string;
+  imageUrl: StaticImageData;
   width: number;
   height: number;
 }
