e77962444660c0e1c27304d7f351552acbb87460
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-04-19 15:19:37 +0900

    Feat: create useWindowSize hook

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+// TODO 추후 전역 상태로 변경하는게 좋지않을까??
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
+  const [isDesktop, setIsDesktop] = useState<boolean>(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      setIsDesktop(window.innerWidth < 1024 ? false : true);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
+  return {
+    windowSize,
+    isDesktop
+  };
+}
