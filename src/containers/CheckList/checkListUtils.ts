fadbb5b104d9db5d87622c4d69edfc6888409ee9
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-04-23 16:41:12 +0900

    Feat: create checkListUtils module

---
+++ src/containers/CheckList/checkListUtils.ts
@@ -0,0 +1,29 @@
+class CheckListUtils {
+  public isOverMaxCheckListCount(checkLists: any) {
+
+  }
+
+  public isValidCharacterName(characterName: string) {
+    if (characterName.length === 0 || characterName.length > 20) {
+      return false;
+    }
+
+    const regex = /^[A-Za-z0-9+]*$/;
+    if (regex.test(characterName) === false) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public isValidNgPlus(ngPlus: number) {
+    if (ngPlus < 0 || ngPlus > 1000) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+const _instance = new CheckListUtils();
+export default _instance;
