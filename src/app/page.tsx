2dec04033066b8bd1204048e4d00e736bf0813f0
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-05-08 16:19:22 +0900

    Feat: prefetch test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import HomeContainer from '@/containers/Home';
 
-export default function Home() {
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  // TODO 시간 설정 확인
+  await queryClient.prefetchQuery({
+    queryKey: ['testFn'],
+    queryFn: getTestFn,
+    // staleTime: 1000,
+    // gcTime: 1000 * 60 * 10,
+  });
+
+
+  async function getTestFn() {
+    try {
+      const response = await fetch('http://localhost:27017/api/item/item-categories');
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      // return await response.json();
+      return 'page에서 preFetch 한 데이터';
+
+    } catch (error) {
+      console.log('에러 체크', error);
+    }
+  }
+
   return (
-    <HomeContainer />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <HomeContainer />
+    </HydrationBoundary>
   );
 }
