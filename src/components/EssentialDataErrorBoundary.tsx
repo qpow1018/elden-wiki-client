32d612c6477e763b0b44930a8f2e92a0d79f27b0
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-05-11 17:30:42 +0900

    Feat: create EssentialDataErrorBoundary

---
+++ src/components/EssentialDataErrorBoundary.tsx
@@ -0,0 +1,57 @@
+import { Box } from '@mui/material';
+
+import BoxLoading from '@/components/Loading/BoxLoading';
+
+export default function EssentialDataErrorBoundary(
+  props: {
+    isLoading: boolean;
+    isError: boolean;
+    loadingComponent?: React.ReactNode;
+    children: React.ReactNode;
+  }
+) {
+  return (
+    <>
+      { props.isLoading === true &&
+        <LoadingComponent
+          loadingComponent={props.loadingComponent}
+        />
+      }
+
+      { props.isLoading === false &&
+        <>
+          {props.isError === false ? (
+            <>{ props.children }</>
+          ) : (
+            <ErrorComponent />
+          )}
+        </>
+      }
+    </>
+  );
+}
+
+function LoadingComponent(
+  props: {
+    loadingComponent?: React.ReactNode;
+  }
+) {
+  return (
+    <>
+      { props.loadingComponent !== undefined ? (
+        props.loadingComponent
+      ) : (
+        <BoxLoading />
+      )}
+    </>
+  );
+}
+
+// TODO 적당한 디자인 / 홈으로, 새로고침
+function ErrorComponent() {
+  return (
+    <Box>
+      페이지 에러
+    </Box>
+  );
+}
