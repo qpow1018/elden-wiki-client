d93938fdfb59872a746ee2b9443ae6830cf3eb06
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2024-05-13 15:17:48 +0900

    Feat: update Item page

--- src/containers/Item/index.tsx
+++ src/containers/Item/index.tsx
@@ -1,55 +1,87 @@
 'use client';
 
 import { Box } from '@mui/material';
+import theme from '@/styles/theme';
 
 import { useGetMainCategory } from '@/queries';
 import { ResItemMainCategory, ResItemSubCategory } from '@/types/api';
 
 import Layout from '@/components/Layout';
+import Container from '@/components/Base/Container';
 import EssentialDataErrorBoundary from '@/components/EssentialDataErrorBoundary';
+import Text from '@/components/Base/Text';
+import Link from '@/components/Base/Link';
+
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function Item() {
   const { data: itemCategories, isLoading, isError } = useGetMainCategory<ResItemMainCategory[]>([]);
 
   return (
     <Layout>
-      <EssentialDataErrorBoundary isLoading={isLoading} isError={isError}>
-        <Box sx={{ padding: '16px' }}>
-          <Box sx={{ marginBottom: '8px' }}>아이템 DB 목록</Box>
-          { itemCategories.map(item =>
+      <Container>
+        <EssentialDataErrorBoundary isLoading={isLoading} isError={isError}>
+          <Box
+            sx={{
+              borderBottom: `1px solid ${theme.color.border.default}`,
+              padding: '16px',
+            }}
+          >
+            <Text
+              sx={{
+                fontSize: '12px',
+                color: theme.color.text.dark,
+              }}
+            >
+              아이템 DB 목록
+            </Text>
+          </Box>
+
+          { itemCategories.map((item, index) =>
             <ItemMainCategoryItem
               key={item.id}
-              name={item.name}
-              subCategories={item.subCategories}
+              href={`/item/category/${item.categoryNo}`}
+              text={`${index + 1}. ${item.name}`}
             />
           )}
-        </Box>
-      </EssentialDataErrorBoundary>
+        </EssentialDataErrorBoundary>
+      </Container>
     </Layout>
   );
 }
 
 function ItemMainCategoryItem(
   props: {
-    name: string;
-    subCategories: ResItemSubCategory[];
+    href: string;
+    text: string;
   }
 ) {
   return (
-    <Box
+    <Link
+      href={props.href}
       sx={{
-        boxShadow: '0 0 1px red'
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderBottom: `1px solid ${theme.color.border.dark}`,
+        paddingRight: '4px',
+        height: 44,
+        padding: '0 16px',
       }}
     >
-      <Box>이름 : { props.name }</Box>
-
-      { props.subCategories.map(item =>
-        <Box
-          key={item.id}
-        >
-          서브 - { item.name } - 라우팅 필요
-        </Box>
-      )}
-    </Box>
+      <Text
+        sx={{
+          fontSize: '13px',
+        }}
+      >
+        { props.text }
+      </Text>
+      <KeyboardArrowRightIcon
+        sx={{
+          fontSize: '18px',
+          color: theme.color.text.dark,
+        }}
+      />
+    </Link>
   );
 }
